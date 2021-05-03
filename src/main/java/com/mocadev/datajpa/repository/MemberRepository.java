e56fcb97c7e51518f4d5e7af111f1187a036c813
tony <chcjswo@gmail.com>
2021-05-04 00:23:37 +0900

    파라미터 바인딩 학습

--- src/main/java/com/mocadev/datajpa/repository/MemberRepository.java
+++ src/main/java/com/mocadev/datajpa/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package com.mocadev.datajpa.repository;
 
 import com.mocadev.datajpa.dto.MemberDto;
 import com.mocadev.datajpa.entity.Member;
+import java.util.Collection;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -26,5 +27,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
 	@Query("select new com.mocadev.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
 	List<MemberDto> findMemberDto();
+
+	@Query("select m from Member m where m.username in :names")
+	List<Member> findByNames(@Param("names") Collection<String> names);
 
 }
--- src/test/java/com/mocadev/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/com/mocadev/datajpa/repository/MemberRepositoryTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.mocadev.datajpa.dto.MemberDto;
 import com.mocadev.datajpa.entity.Member;
 import com.mocadev.datajpa.entity.Team;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -97,6 +98,19 @@ class MemberRepositoryTest {
 
 		for (MemberDto dto : memberDto) {
 			System.out.println("dto = " + dto);
+		}
+	}
+
+	@Test
+	void findNamesTest() {
+		Member m1 = new Member("AAA", 20);
+		Member m2 = new Member("BBB", 10);
+		memberRepository.save(m1);
+		memberRepository.save(m2);
+
+		List<Member> result = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+		for (Member member : result) {
+			System.out.println("member = " + member);
 		}
 	}
 
